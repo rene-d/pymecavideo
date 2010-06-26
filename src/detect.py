565886682481adbdaa0607a810928cea4e454000
djibb <ashashiwa@gmail.com>
2010-06-26 19:55:46 +0000

    motion detection

---
+++ src/detect.py
@@ -0,0 +1,38 @@
+import cv
+from cv import *
+
+def filter_picture(part,image):
+    if type(image)==type("") and type(part)==type("") :
+        image=cv.LoadImage(image,1)
+        part=cv.LoadImage(part,1)
+        point1, point2 = detect_part(part,image)
+        #print point1, point2
+        return point2
+    elif "iplimage" in str(type(part)) and "iplimage" in str(type(image)):
+        points = detect_part(part,image)
+        return points
+    elif "QImage" in str(type(part)) and "QImage" in str(type(image)):
+        part.save("part.png")
+        image.save("image.png")
+        image=cv.LoadImage("image.png",1)
+        part=cv.LoadImage("part.png",1)
+        point1, point2 = detect_part(part,image)
+        return point2
+        
+    else :
+        return "Type Error"
+
+def detect_part(part,image):
+
+    resultW = image.width - part.width + 1
+    resultH = image.height - part.height +1
+    #print resultW, resultH
+    result = cv.CreateImage((resultW, resultH), IPL_DEPTH_32F, 1)
+    cv.MatchTemplate(image, part,result, cv.CV_TM_SQDIFF)
+    m, M, point2, point1 = cv.MinMaxLoc(result)
+    return point1, point2
+
+#filter_picture("modele.png", "cible.png")
+#image=cv.LoadImage("modele.png",1)
+#icon=cv.LoadImage("cible.png",1)
+#filter_picture(image, icon)
---
+++ src/label_auto.py
@@ -0,0 +1,84 @@
+#-*- coding: utf-8 -*-
+
+"""
+    videotraj, a module for pymecavideo:
+      a program to track moving points in a video frameset
+      
+    Copyright (C) 2007 Jean-Baptiste Butet <ashashiwa@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import sys, os, thread, time, commands
+from PyQt4 import QtCore, QtGui
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from vecteur import vecteur
+
+class Label_Auto(QLabel):
+    def __init__(self, parent,app):
+        """make a rectangle near point to be tracked"""
+        QLabel.__init__(self, parent)
+        self.parent = parent
+        self.setGeometry(QRect(0,0,640,480))
+        self.setAutoFillBackground(False)
+        self.app = app
+        self.setCursor(Qt.CrossCursor)
+        self.setMouseTracking(True)
+    def mousePressEvent(self, event):
+        self.setMouseTracking(False)
+        self.x_1 = event.x()
+        self.x_2 = event.x()
+        self.y_1 = event.y()
+        self.y_2 = event.y()
+        
+    def mouseMoveEvent(self, event):
+        x= event.x()
+        y = event.y()
+
+        if not self.hasMouseTracking():
+            if x > self.x_1 :
+                self.x_2 = x
+            elif x< self.x_1:
+                self.x_1 = x
+            if y > self.y_1 :
+                self.y_2 = y
+            elif y< self.y_1:
+                self.y_1 = y
+        self.app.label_video.zoom_croix.show()
+        self.pos=vecteur(x,y)
+        self.app.label_video.fait_crop(self.pos)
+        self.app.ui.label_zoom.setPixmap(self.app.label_video.cropX2)
+        self.update()
+    def mouseReleaseEvent(self,event):
+        self.app.zoom = True
+        rectangle = QRect(self.x_1,self.y_1,self.x_2-self.x_1,self.y_2-self.y_1)
+        self.app.motif = self.app.image_640_480.copy(rectangle)
+        self.app.emit(QtCore.SIGNAL('selection_done()'))
+
+        self.hide()
+
+        
+            
+    def paintEvent(self, event):
+        if not self.hasMouseTracking():
+            painter = QPainter()
+            painter.begin(self)
+            painter.setPen(Qt.green)
+            #painter.setBrush(Qt.Dense6Pattern)
+            painter.drawRect(self.x_1,self.y_1,self.x_2-self.x_1,self.y_2-self.y_1)
+            painter.end()
+
+
+
