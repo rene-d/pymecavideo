668569204582fa85f4540f4b14ba13e8029ebb4a
cedrick-f <cedrick.faury@laposte.net>
2015-04-17 10:49:44 +0200

    Finalisation du script "setup_win.py" Signed-off-by: cedrick-f <cedrick.faury@laposte.net>

---
+++ src/setup_win.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+##################################################################################################
+#
+#    Script pour générer un pack avec executable :
+#    c:\python27\python setup_win.py build
+#
+##################################################################################################
+
+import sys, os
+from glob import glob
+from cx_Freeze import setup, Executable
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+    
+## Remove the build folder, a bit slower but ensures that build contains the latest
+import shutil
+shutil.rmtree("build", ignore_errors=True)
+
+# Inculsion des fichiers de donn�es
+#################################################################################################
+includefiles = [('D:/Developpement/Microsoft.VC90.CRT', "Microsoft.VC90.CRT"),
+                     '../AUTHORS', '../COPYING', '../README.fr', 
+                     ('../data', "data"),
+                     ('mencoder.exe', "mencoder.exe"),
+                     'opencv_ffmpeg2411.dll']
+
+#includefiles.extend(glob(r"*.xlsx"))
+#includefiles.extend(glob(r"*.xls"))
+#includefiles.extend(glob(r"*.xlsm"))
+#for p in ['C:\\Python27\\Lib\site-packages\\html5lib',
+#            'C:\\Python27\\Lib\\site-packages\\xhtml2pdf',
+#            'C:\\Python27\\Lib\\site-packages\\PIL']:
+#    if not p in sys.path:
+#        sys.path.append(p)
+
+#sys.path = ['', 'C:\\Python27\\Lib\\idlelib', 
+#            'C:\\Python27\\lib\\site-packages\\setuptools-3.6-py2.7.egg', 
+#            'C:\\Python27\\lib\\site-packages\\xmind-0.1a.0-py2.7.egg', 
+#            'C:\\Python27\\lib\\site-packages\\distribute-0.7.3-py2.7.egg', 
+#            'C:\\Python27\\lib\\site-packages\\six-1.8.0-py2.7.egg', 
+#            'C:\\Python27\\lib\\site-packages\\python_dateutil-2.2-py2.7.egg', 
+#            'C:\\Python27\\lib\\site-packages\\comtypes-1.1.1-py2.7.egg', 
+#            'C:\\Python27\\lib\\site-packages\\xlutils-1.7.1-py2.7.egg', 
+#            'C:\\Python27\\lib\\site-packages\\pillow-2.7.0-py2.7-win32.egg', 
+#            'C:\\Python27\\lib\\site-packages\\html5lib-1.0b3-py2.7.egg', 
+#            'C:\\Python27\\lib\\site-packages\\xhtml2pdf-0.0.6-py2.7.egg', 
+#            'C:\\Python27\\python27.zip', 'C:\\Python27\\DLLs', 
+#            'C:\\Python27\\lib', 
+#            'C:\\Python27\\lib\\plat-win', 
+#            'C:\\Python27\\lib\\lib-tk', 
+#            'C:\\Python27', 
+#            'C:\\Python27\\lib\\site-packages', 
+#            'C:\\Python27\\lib\\site-packages\\win32', 
+#            'C:\\Python27\\lib\\site-packages\\win32\\lib', 
+#            'C:\\Python27\\lib\\site-packages\\Pythonwin', 
+#            'C:\\Python27\\lib\\site-packages\\wx-3.0-msw',
+#            'C:\\Python27\\Lib\site-packages\\html5lib',
+#            'C:\\Python27\\Lib\\site-packages\\xhtml2pdf',
+#            'C:\\Python27\\Lib\\site-packages\\Pillow-2.7.0-py2.7-win32\\PIL']
+
+
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {'build_exe': 'build',
+                     "packages": ["os", "cv2"], 
+                     "includes": [],
+                     "optimize" : 0,
+#                     "path" : ["../packages/html5lib"],#, "../packages/xhtml2pdf",  "../packages/xhtml2pdf/w3c"],
+#                     "zip_includes": [("../packages/html5lib/", "html5lib"),
+#                                      ("../packages/xhtml2pdf/w3c/css.py", "xhtml2pdf/w3c/css.py")],
+                     "excludes": ["tkinter",
+                                  '_gtkagg', '_tkagg', 'bsddb', 'curses', 'pywin.debugger',
+                                  'pywin.debugger.dbgcon', 'pywin.dialogs', 'tcl',
+                                  'Tkconstants', 'pydoc', 'doctest', 'test', 'sqlite3',
+                                  "matplotlib", 
+                                  "PIL", "scipy", "email"
+                                  #'PyQt4.QtWebKit', #'PyQt4.QtOpenGL', #'PyQt4.QtNetwork'#♣,'PyQt4.QtSvg'
+                                  ],
+                     "include_files": includefiles,
+                     'bin_excludes' : ['libgdk-win32-2.0-0.dll', 'libgobject-2.0-0.dll', 'tcl85.dll',
+                                        'tk85.dll', "UxTheme.dll", "mswsock.dll", "POWRPROF.dll",
+                                        'QtWebKit4.dll', 'QtOpenGL4.dll', 'QtNetwork4.dll', #♣'QtSvg4.dll',
+                                        "pywintypes27.dll"
+                                      ]}
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+
+cible = Executable(
+    script = "pymecavideo.py",
+    base = base,
+    compress = True,
+    icon = os.path.join("", '../data/icones/pymecavideo.ico'),
+    initScript = None,
+    copyDependentFiles = True,
+    appendScriptToExe = False,
+    appendScriptToLibrary = False
+    )
+
+
+setup(  name = "pymecavideo",
+        version = "6.2.3.0",
+        author = 'Jean-Baptiste Butet ; Georges Khaznadar',
+        description = u"pymecavideo",
+        options = {"build_exe": build_exe_options},
+#        include-msvcr = True,
+        executables = [cible])
