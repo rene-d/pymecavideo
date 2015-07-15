2fe9a47da52a93841d48d7195c28510855b2a4e8
jb <jb@localhost.localdomain>
2015-07-15 14:13:58 +0200

    Nettoyage de code

--- src/toQimage.py
+++ src/toQimage.py
@@ -4,31 +4,16 @@ import numpy as np
 class NotImplementedException:
     pass
  
-gray_color_table = [qRgb(i, i, i) for i in range(256)]
- 
+
 def toQImage(im, copy=False):
     if im is None:
         return QImage()
  
     if im.dtype == np.uint8:
-        if len(im.shape) == 2:
-            print('1')
-            qim = QImage(im.data, im.shape[1], im.shape[0], im.strides[0], QImage.Format_Indexed8)
-            qim.setColorTable(gray_color_table)
-            return qim.copy() if copy else qim
- 
-        elif len(im.shape) == 3:
-            print('2',len(im.shape) )
+        if len(im.shape) == 3:
             if im.shape[2] == 3:
-                #print('3', im.shape,  im.shape[1],  im.shape[2], im.strides)
-                print(dir(im))
-                #qim = QImage(im.data, im.shape[1], im.shape[0], im.strides[0], QImage.Format_RGB888);
                 qim = QImage(im.data, im.shape[1], im.shape[0], QImage.Format_RGB888);
                 return qim.copy() if copy else qim
-            elif im.shape[2] == 4:
-                print('4')
-                qim = QImage(im.data, im.shape[1], im.shape[0], im.strides[0], QImage.Format_ARGB32);
-                return qim.copy() if copy else qim
- 
+
     raise NotImplementedException
 
