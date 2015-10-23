bb3dbb06dd079653b0d7e3c944cb207fcd1b224a
Jean-Baptiste BUTET <ashashiwa@gmail.com>
2015-10-23 12:31:29 +0200

    Remise de la détection automatique de plusieurs points

--- src/pymecavideo.py
+++ src/pymecavideo.py
@@ -89,6 +89,54 @@ _encoding = QApplication.UnicodeUTF8
 def _translate(context, text, disambig):
     return QApplication.translate(context, text, disambig, _encoding)
 
+class MonThreadDeCalcul(threading.Thread):
+    """Thread permettant le calcul des points automatiquement. Version Python"""
+
+    def __init__(self, parent, motif, image, dossTemp):
+        threading.Thread.__init__(self)
+        self.parent = parent
+        self.parent.dbg.p(1, "rentre dans 'monThreadDeCalcul'")
+        self.motif = motif
+        self.image = image
+        self.dossTemp = dossTemp
+        self._stopevent = threading.Event()
+
+    def run(self):
+        """
+        lance le thread.
+        """
+        while not self._stopevent.isSet():
+            self.parent.picture_detect(self.dossTemp)
+            self._stopevent.wait(0.01)
+        os.remove(self.dossTemp)
+
+    def stop(self):
+        self._stopevent.set()
+
+
+class MonThreadDeCalculQt(QThread):
+    """Thread permettant le calcul des points automatiquement. Version Qt. 20/04/2015 : focntionne mal sous windows"""
+
+    def __init__(self, parent, motif, image):
+        QThread.__init__(self)
+        self.parent = parent
+        self.parent.dbg.p(1, "rentre dans 'monThreadDeCalcul'")
+        self.motif = motif
+        self.image = image
+        self.stopped = False
+
+    def run(self):
+        """
+        lance le thread.
+        stocke les corrdonnées des points trouvés
+        Envoi un signal quand terminé.
+        """
+        while not self.stopped:
+            self.parent.picture_detect()
+
+    def stop(self):
+        self.stopped = True
+
 
 class StartQT4(QMainWindow):
     def __init__(self, parent=None, opts=[], args=[]):
@@ -194,6 +242,80 @@ class StartQT4(QMainWindow):
 
         # chargement d'un éventuel premier fichier
         self.splashVideo()
+
+    def stopComputing(self):
+        self.dbg.p(1, "rentre dans 'stopComputing'")
+        self.monThread.stop()
+        del self.monThread
+        self.label_video.setEnabled(1)
+        self.ui.pushButton_video.setEnabled(0)
+        self.ui.pushButton_video.hide()
+
+    def onePointFind(self):
+        """est appelée quand un point a été trouvé lors de la détection automatique
+        self.pointFound : liste des points trouvés
+        """
+        self.dbg.p(1, "rentre dans 'onePointFind'")
+
+        self.pointsFound.append(self.pointTrouve)  # stock all points found
+
+        for point in self.pointsFound:
+            self.label_video.storePoint(vecteur(point[0], point[1]))
+
+
+    def storeMotif(self):
+        self.dbg.p(1, "rentre dans 'storeMotif'")
+        if len(self.motif) == self.nb_de_points:
+            self.dbg.p(3, "selection des motifs finie")
+            self.label_auto.hide()
+            self.label_auto.close()
+            self.indexMotif = 0
+
+            self.ui.pushButton_video.setText("STOP CALCULS")
+            self.ui.pushButton_video.setEnabled(1)
+            self.ui.pushButton_video.show()
+
+            self.label_video.setEnabled(0)
+            self.goCalcul = True
+
+            # TODO : tests avec les différents mode de threading
+            ##Qthread (fonctionne mal)
+            # self.monThread = MonThreadDeCalculQt(self, self.motif[Motif], self.imageAffichee)
+
+            # Python
+            dossTemp = tempfile.NamedTemporaryFile(delete=False).name
+            self.monThread = MonThreadDeCalcul(self, self.motif[self.indexMotif], self.imageAffichee, dossTemp)
+            self.monThread.start()
+
+
+    def picture_detect(self, dossTemp):
+        """
+        Est lancée lors de la détection automatique des points. Gère l'ajout des thread de calcul.
+        self.myThreads : tableau contenant les thread
+        self.motifs : tableau des motifs
+
+        """
+        self.dbg.p(1, "rentre dans 'picture_detect'")
+        self.dbg.p(3, "début 'picture_detect'" + str(self.indexMotif))
+        if self.index_de_l_image <= self.image_max:
+            self.pointsFound = []
+            if self.indexMotif <= len(self.motif) - 1:
+                self.dbg.p(1, "'picture_detect' : While")
+                self.pointTrouve = filter_picture(self.motif, self.indexMotif, self.imageAffichee, dossTemp)
+                self.dbg.p(3, "Point Trouve dans mon Thread : " + str(self.pointTrouve))
+                self.onePointFind()
+
+                self.indexMotif += 1
+            else:
+                self.indexMotif = 0
+
+        if self.index_de_l_image == self.image_max:
+            if self.indexMotif == 0 and not self.goCalcul:  # dernier passage
+                self.emit(SIGNAL('stopCalculs()'))
+            elif self.indexMotif == 0 and self.goCalcul:  # premier passage, premier calcul de la dernière image
+                self.goCalcul = False
+
+
 
     def sizeHint(self):
         return QSize(1024, 800)
