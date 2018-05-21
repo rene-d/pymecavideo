ec7b4a0f0b4dfc551871eaaf9c6e10683bcbe59f
Georges Khaznadar <georgesk@debian.org>
2018-05-21 19:48:44 +0200

    ported to Python3

--- src/testfilm_prop_ced.py
+++ src/testfilm_prop_ced.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+
 import sys
 import threading
 import os.path
@@ -30,21 +32,21 @@ class film:
         self.ok = False
         try:
             self.data = cv.GetCaptureProperty(self.capture, cv.CV_CAP_PROP_POS_MSEC)
-            print "CV_CAP_PROP_POS_MSEC : ", self.data
+            print ("CV_CAP_PROP_POS_MSEC : ", self.data)
             self.data = cv.GetCaptureProperty(self.capture, cv.CV_CAP_PROP_POS_FRAMES)
-            print "CV_CAP_PROP_POS_FRAMES : ", self.data
+            print ("CV_CAP_PROP_POS_FRAMES : ", self.data)
             self.data = cv.GetCaptureProperty(self.capture, cv.CV_CAP_PROP_POS_AVI_RATIO)
-            print "CV_CAP_PROP_POS_AVI_RATIO : ", self.data
+            print ("CV_CAP_PROP_POS_AVI_RATIO : ", self.data)
             self.data = cv.GetCaptureProperty(self.capture, cv.CV_CAP_PROP_FRAME_WIDTH)
-            print "CV_CAP_PROP_FRAME_WIDTH : ", self.data
+            print ("CV_CAP_PROP_FRAME_WIDTH : ", self.data)
             self.data = cv.GetCaptureProperty(self.capture, cv.CV_CAP_PROP_FRAME_HEIGHT)
-            print "CV_CAP_PROP_FRAME_HEIGHT : ", self.data
+            print ("CV_CAP_PROP_FRAME_HEIGHT : ", self.data)
             self.data = cv.GetCaptureProperty(self.capture, cv.CV_CAP_PROP_FPS)
-            print "CV_CAP_PROP_FPS : ", self.data
+            print ("CV_CAP_PROP_FPS : ", self.data)
             self.data = cv.GetCaptureProperty(self.capture, cv.CV_CAP_PROP_FOURCC)
-            print "CV_CAP_PROP_FOURCC : ", self.data
+            print ("CV_CAP_PROP_FOURCC : ", self.data)
             self.data = cv.GetCaptureProperty(self.capture, cv.CV_CAP_PROP_FRAME_COUNT)
-            print "CV_CAP_PROP_FRAME_COUNT : ", self.data
+            print ("CV_CAP_PROP_FRAME_COUNT : ", self.data)
             self.ok = True
         except:
             pass
