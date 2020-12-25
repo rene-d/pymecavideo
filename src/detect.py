a34536140a39c97e6703db4d6aab6be57139e51d
JB BUTET <ashashiwa@gmail.com>
2020-12-25 12:36:16 +0100

    Changement de la méthode de conversiont de QImage en image CV.-> gain de performances

--- src/detect.py
+++ src/detect.py
@@ -21,7 +21,7 @@
 """
 
 import os.path
-import tempfile
+import tempfile, time
 
 import cv2
 import numpy as np
@@ -29,14 +29,31 @@ import numpy as np
 from globdef import *
 
 
-def filter_picture(parts, num, image, dossTemp, points=None):
+import functools
+
+
+def time_it(func):
+    """Timestamp decorator for dedicated functions"""
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.time()                 
+        result = func(*args, **kwargs)
+        elapsed = time.time() - start
+        mlsec = repr(elapsed).split('.')[1][:3]
+        readable = time.strftime("%H:%M:%S.{}".format(mlsec), time.gmtime(elapsed))
+        print('Function "{}": {} sec'.format(func.__name__, readable))
+        return result
+    return wrapper
+
+
+#@time_it
+def filter_picture(parts, num, image, points=None):
     """
     Trouve la position d'une image partielle dans une image donnée
     @param parts une liste d'images partielles
     @param num un index signalant l'image active (couramment seul l'index zéro
     est supporté)
     @param image l'image où on va chercher à trouver le motif partiel
-    @param dossTemp un dossier temporaire pour des conversions éventuelles
     @param points liste de points près desquels il est plus vraisemblable
     de trouver le motif.
     @return les coordonnées d'un point, résultant du suivi automatique du
@@ -48,10 +65,10 @@ def filter_picture(parts, num, image, dossTemp, points=None):
         point = points[num]
     else:
         point=None
-        
+
     if "QImage" in str(type(part)):
-        part = QImage2CVImage(part, dossTemp)
-        
+        part = QImage2CVImage(part)
+
     if type(image) == type("") and type(part) == type(""):
         image = cv2.imread(image, 1)
         part = cv2.imread(part, 1)
@@ -63,14 +80,13 @@ def filter_picture(parts, num, image, dossTemp, points=None):
         return (point[0]+part.shape[1]/2, point[1]+part.shape[0]/2)
     
     elif "QImage" in str(type(image)):
-        image = QImage2CVImage(image, dossTemp)
-        point = detect_part(part, image, point)
-
+        image = QImage2CVImage(image) #TODO 110ms
+        point = detect_part(part, image, point) #TODO 130ms
         return (point[0]+part.shape[1]/2, point[1]+part.shape[0]/2)
 
     else:
         return "Type Error"
-
+#@time_it
 def gaussMatrix(forme, sommet, hauteur, largeur, inverse=False):
     """
     renvoie une matrice de valeurs qui forment un pic gaussien autour
@@ -92,7 +108,7 @@ def gaussMatrix(forme, sommet, hauteur, largeur, inverse=False):
         result=maxv-result
     return result
 
-
+#@time_it
 def detect_part(part, image, point=None):
     """
     Détecte une image partielle dans une image complète
@@ -103,9 +119,9 @@ def detect_part(part, image, point=None):
     @return l'emplacement où se trouve le motif recherché
     """
     import numpy as np
-    
     ##méthode 1 : 
-    result = cv2.matchTemplate(image, part, cv2.TM_SQDIFF) #  cv2.TM_CCOEFF
+    result = cv2.matchTemplate(image, part, cv2.TM_SQDIFF) #  cv2.TM_CCOEFF  Pour AMELIORER la vitesse, une possiblité est de réduire "image"
+    
     ##########################################################
     #À ce point, result est une carte des coïncidences possibles
     #entre le motif "part" et l'image complète "image"
@@ -116,21 +132,35 @@ def detect_part(part, image, point=None):
         flou=2
         vraisemblable=gaussMatrix(result.shape,point,np.amax(result),flou*(part.shape[0]+part.shape[1]), inverse=True)
         result = result+vraisemblable
+
     ########## ceci minimise les chances de trouver loin ###########
     m, M, minloc, maxloc = cv2.minMaxLoc(result)
     return minloc
     
-    ##méthode2 : découpe arbitrairementet si
+ 
+#@time_it
+#def QImage2CVImage_(img, dossierTemp):
+    #""" Convertit une QImage en une image CV
+    #"""
+    #fichImg = os.path.join(dossierTemp + "image.png")
+    #img.save(fichImg) #100ms
+    #img_cv = cv2.imread(fichImg, 1)
+    #os.remove(fichImg)
+    #return img_cv
     
-    
+
+def QImage2CVImage(incomingImage):
+    '''  Converts a QImage into an opencv MAT format  '''
+
+    incomingImage = incomingImage.convertToFormat(4)
+
+    width = incomingImage.width()
+    height = incomingImage.height()
+
+    ptr = incomingImage.bits()
+    ptr.setsize(incomingImage.byteCount())
+    arr = np.array(ptr).reshape(height, width, 4)  #  Copies the data
+    return arr
 
 
 
-def QImage2CVImage(img, dossierTemp):
-    """ Convertit une QImage en une image CV
-    """
-    fichImg = os.path.join(dossierTemp + "image.png")
-    img.save(fichImg)
-    img = cv2.imread(fichImg, 1)
-    os.remove(fichImg)
-    return img
