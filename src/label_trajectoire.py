c4477d0a2f5418f35853b8dd65a8ee4eb48701ac
Georges Khaznadar <georgesk@debian.org>
2022-09-23 17:25:29 +0200

    used properties when necessary, round values before drawing on the screen

--- src/label_trajectoire.py
+++ src/label_trajectoire.py
@@ -87,8 +87,9 @@ class Label_Trajectoire(QLabel):
                         if self.label_video.app.ui.radioButtonNearMouse.isChecked() and self.pos_souris != None:
                             near = 20
                             pos = self.pos_souris
-                            distance = QPoint(point.x() + self.origine.x() - ptreferentiel.x(),
-                                              point.y() + self.origine.y() - ptreferentiel.y()) - pos
+                            distance = QPoint(
+                                point.x + self.origine.x - ptreferentiel.x,
+                                point.y + self.origine.y - ptreferentiel.y) - pos
                             if distance.manhattanLength() < near:
                                 self.label_video.app.dbg.p(
                                     2, "mouse near a point")
@@ -107,20 +108,18 @@ class Label_Trajectoire(QLabel):
                                         self.label_video.app.points[key + 1][0])/self.label_video.app.deltaT  # en "delta_T"
                                     tempsBefore = float(
                                         self.label_video.app.points[key-1][0])/self.label_video.app.deltaT  # en "delta_T"
-                                    pointBefore = QPoint(
-                                        (self.label_video.app.points[key - 1][i].x() + self.origine.x(
-                                        ) - ptreferentielBefore.x())/(tempsAfter-tempsBefore),
-                                        (self.label_video.app.points[key - 1][i].y() + self.origine.y() - ptreferentielBefore.y())/(tempsAfter-tempsBefore))
+                                    pointBefore = vecteur(
+                                        (self.label_video.app.points[key - 1][i].x + self.origine.x - ptreferentielBefore.x)/(tempsAfter-tempsBefore),
+                                        (self.label_video.app.points[key - 1][i].y + self.origine.y - ptreferentielBefore.y)/(tempsAfter-tempsBefore))
 
-                                    pointAfter = QPoint(
-                                        (self.label_video.app.points[key + 1][i].x() + self.origine.x(
-                                        ) - ptreferentielAfter.x())/(tempsAfter-tempsBefore),
-                                        (self.label_video.app.points[key + 1][i].y() + self.origine.y() - ptreferentielAfter.y())/(tempsAfter-tempsBefore))
+                                    pointAfter = vecteur(
+                                        (self.label_video.app.points[key + 1][i].x + self.origine.x - ptreferentielAfter.x)/(tempsAfter-tempsBefore),
+                                        (self.label_video.app.points[key + 1][i].y + self.origine.y - ptreferentielAfter.y)/(tempsAfter-tempsBefore))
 
                                     vector_speed = pointAfter - pointBefore
 
-                                    self.speedToDraw.append((QPoint(point.x() + self.origine.x() - ptreferentiel.x(),
-                                                                    point.y() + self.origine.y() - ptreferentiel.y()),
+                                    self.speedToDraw.append((vecteur(point.x + self.origine.x - ptreferentiel.x,
+                                                                    point.y + self.origine.y - ptreferentiel.y),
                                                              vector_speed, i))
                                 except KeyError:  # last point -> can't compute speed
                                     pass
@@ -150,12 +149,14 @@ class Label_Trajectoire(QLabel):
         # paint the origin
         if not self.chrono == 2:
             self.painter.setPen(Qt.green)
-            self.painter.drawLine(self.origine_mvt.x() - 5, self.origine_mvt.y(), self.origine_mvt.x() + 5,
-                                self.origine_mvt.y())
-            self.painter.drawLine(self.origine_mvt.x(), self.origine_mvt.y() - 5, self.origine_mvt.x(),
-                                self.origine_mvt.y() + 5)
-            self.painter.drawText(self.origine_mvt.x(),
-                                self.origine_mvt.y() + 15, "O")
+            self.painter.drawLine(
+                round(self.origine_mvt.x) - 5, round(self.origine_mvt.y),
+                round(self.origine_mvt.x) + 5, round(self.origine_mvt.y))
+            self.painter.drawLine(
+                round(self.origine_mvt.x), round(self.origine_mvt.y) - 5,
+                round(self.origine_mvt.x), round(self.origine_mvt.y) + 5)
+            self.painter.drawText(
+                round(self.origine_mvt.x), round(self.origine_mvt.y) + 15, "O")
             self.painter.end()
 
         # peint les informations pour le mode chronophotographie
@@ -193,25 +194,16 @@ class Label_Trajectoire(QLabel):
             # dessine l'échelle
             if self.chrono == 2:  # chronogramme
                 if self.label_video.app.echelle_faite : #dessine une échelle en haut, horizontalement
-                    longueur = sqrt((self.label_video.echelle_image.p1.x()-self.label_video.echelle_image.p2.x(
-                    ))**2 + (self.label_video.echelle_image.p1.y()-self.label_video.echelle_image.p2.y())**2)
+                    longueur = round((self.label_video.echelle_image.p1 - self.label_video.echelle_image.p2).norme)
                     self.painter.drawLine(x1, y1-10, x1, y1+10)
                     self.painter.drawLine(x1, y1, longueur+x1, y1)
                     self.painter.drawLine(longueur+x1, y1-10, longueur+x1, y1+10)
-                    try :
-                        text = unicode("d = {0:.2e} m").format(
-                            self.label_video.echelle_image.longueur_reelle_etalon)
-                    except NameError:
-                        text = "d = {0:.2e} m".format(
-                            self.label_video.echelle_image.longueur_reelle_etalon)   
+                    text = "d = {0:.2e} m".format(self.label_video.echelle_image.longueur_reelle_etalon)   
                     font_metrics = QFontMetrics(font)
                     text_width = font_metrics.width(text)
-                    self.painter.drawText(max(x1+(longueur/2)-(text_width/2), 0), y1+30, text) 
+                    self.painter.drawText(max(x1+round((longueur/2)-(text_width/2)), 0), y1+30, text) 
                 else : #échelle non faite
-                    try : 
-                        self.painter.drawText(x1, y1+20, unicode("échelle non précisée"))
-                    except NameError: 
-                        self.painter.drawText(x1, y1+20, "échelle non précisée")
+                    self.painter.drawText(x1, y1+20, "échelle non précisée")
                 self.painter.end()
 
             ############################################################
@@ -225,19 +217,21 @@ class Label_Trajectoire(QLabel):
                 pen.setWidth(3)
                 self.painter.setPen(pen)
                 if self.label_video.app.echelle_faite: 
-                    self.painter.drawLine(self.label_video.app.label_echelle_trace.p1.x(), self.label_video.app.label_echelle_trace.p1.y(
-                    ), self.label_video.app.label_echelle_trace.p2.x(), self.label_video.app.label_echelle_trace.p2.y())
+                    self.painter.drawLine(
+                        round(self.label_video.app.label_echelle_trace.p1.x),
+                        round(self.label_video.app.label_echelle_trace.p1.y),
+                        round(self.label_video.app.label_echelle_trace.p2.x),
+                        round(self.label_video.app.label_echelle_trace.p2.y))
 
                     echelle = "d = {0:.2e} m".format(
                             self.label_video.echelle_image.longueur_reelle_etalon)
 
-                    self.painter.drawText(self.label_video.app.label_echelle_trace.p1.x(
-                    ), int((self.label_video.app.label_echelle_trace.p1.y()+self.label_video.app.label_echelle_trace.p2.y())/2)+20, echelle)
+                    self.painter.drawText(
+                        round(self.label_video.app.label_echelle_trace.p1.x),
+                        round((self.label_video.app.label_echelle_trace.p1.y + self.label_video.app.label_echelle_trace.p2.y)/2)+20,
+                        echelle)
                 else : #pas d'échelle 
-                    try : 
-                        self.painter.drawText(x1, y1+20, unicode("échelle non précisée"))
-                    except NameError: 
-                        self.painter.drawText(x1, y1+20, "échelle non précisée")
+                    self.painter.drawText(x1, y1+20, "échelle non précisée")
                     
                 self.painter.end()
 
@@ -274,15 +268,16 @@ class Label_Trajectoire(QLabel):
                     else :
                         self.painter.setPen(QColor(self.couleurs[color]))
                     self.painter.setFont(QFont("", 10))
-                    self.painter.translate(point.x() + self.origine.x() - ptreferentiel.x(),
-                                           point.y() + self.origine.y() - ptreferentiel.y())
+                    self.painter.translate(
+                        round(point.x + self.origine.x - ptreferentiel.x),
+                        round(point.y + self.origine.y - ptreferentiel.y))
 
                     if self.chrono == 2:
                         self.painter.drawLine(-4, 0, 4, 0)
                         self.painter.drawLine(0, -4, 0, 4)
                         self.painter.translate(-10, +10)
                         decal = -25
-                        if point.x()+decal <0 : 
+                        if point.x + decal < 0 : 
                             decal = 5 
                         self.painter.drawText(decal, 5, "M"+"'"*num_point+str(no))
                     else :
@@ -291,8 +286,9 @@ class Label_Trajectoire(QLabel):
                         self.painter.translate(-10, +10)
                         self.painter.drawText(0, 0, str(color + 1))
                         
-                    self.painter.translate(-point.x() - self.origine.x() + ptreferentiel.x() + 10,
-                                           -point.y() - 10 - self.origine.y() + ptreferentiel.y())
+                    self.painter.translate(
+                        round(-point.x - self.origine.x + ptreferentiel.x) + 10,
+                        round(-point.y - self.origine.y + ptreferentiel.y) - 10)
                     color += 1
                     num_point+=1
                     
@@ -305,11 +301,12 @@ class Label_Trajectoire(QLabel):
             self.painter.setRenderHint(QPainter.Antialiasing)
             self.painter.setPen(Qt.green)
             # self.painter.translate(0,0)
-            self.painter.translate(self.origine_mvt.x(), self.origine_mvt.y())
-            p1 = QPoint(self.label_video.app.sens_X * (-40), 0)
-            p2 = QPoint(self.label_video.app.sens_X * (40), 0)
-            p3 = QPoint(self.label_video.app.sens_X * (36), 2)
-            p4 = QPoint(self.label_video.app.sens_X * (36), -2)
+            self.painter.translate(
+                round(self.origine_mvt.x), round(self.origine_mvt.y))
+            p1 = QPoint(round(self.label_video.app.sens_X * (-40)), 0)
+            p2 = QPoint(round(self.label_video.app.sens_X * (40)), 0)
+            p3 = QPoint(round(self.label_video.app.sens_X * (36)), 2)
+            p4 = QPoint(round(self.label_video.app.sens_X * (36)), -2)
             self.painter.scale(1, 1)
             self.painter.drawPolyline(p1, p2, p3, p4, p2)
             self.painter.rotate(self.label_video.app.sens_X *
@@ -317,7 +314,8 @@ class Label_Trajectoire(QLabel):
             self.painter.drawPolyline(p1, p2, p3, p4, p2)
             self.painter.rotate(self.label_video.app.sens_X *
                                 self.label_video.app.sens_Y * (90))
-            self.painter.translate(-self.origine_mvt.x(), -self.origine_mvt.y())
+            self.painter.translate(
+                round(-self.origine_mvt.x), round(-self.origine_mvt.y))
             self.painter.end()
 
         # paint speed vectors if asked
@@ -331,8 +329,7 @@ class Label_Trajectoire(QLabel):
                     self.painter.setRenderHint(QPainter.Antialiasing)
                     self.painter.setPen(QColor(self.couleurs[i - 1]))
                     try:
-                        speed = sqrt(vector_speed.x() ** 2 + vector_speed.y() ** 2) * float(self.label_video.echelle_image.mParPx()) \
-                            / (2 * self.label_video.app.deltaT) * float(self.label_video.app.ui.checkBoxScale.currentText())
+                        speed = vector_speed.norme * float(self.label_video.echelle_image.mParPx()) / (2 * self.label_video.app.deltaT) * float(self.label_video.app.ui.checkBoxScale.currentText())
                         self.label_video.app.ui.checkBoxScale.setStyleSheet(
                             "background-color:none")
                         path = QPainterPath()
@@ -343,9 +340,8 @@ class Label_Trajectoire(QLabel):
                         path.lineTo(QPointF(speed - 10, 0) + QPointF(0, -10))
                         path.lineTo(speed, 0)
 
-                        angle = atan2(float(vector_speed.y()),
-                                      float(vector_speed.x()))
-                        self.painter.translate(p.x(), p.y())
+                        angle = vector_speed.anglePolaire
+                        self.painter.translate(round(p.x), round(p.y))
                         self.painter.rotate(degrees(angle))
                         self.painter.drawPath(path)
                         # VERIFIER COORDONÉES ICI
