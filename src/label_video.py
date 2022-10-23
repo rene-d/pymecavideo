ff99611ae88050a07c88263b9c00d1f1d94f2c1d
JB BUTET <ashashiwa@gmail.com>
2022-10-23 10:49:55 +0200

    enlevement informations de débogage

--- src/label_video.py
+++ src/label_video.py
@@ -153,7 +153,7 @@ class Label_Video(QLabel):
 
             ############################################################
             # paint the origin
-            longueur_origine = 500
+            longueur_origine = 5
             self.painter.setPen(Qt.green)
             try:
                 self.painter.drawLine(
