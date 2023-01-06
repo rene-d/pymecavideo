c2fd09f79da235f2193346e6192ef592301c71b0
Georges Khaznadar <georgesk@debian.org>
2023-01-06 22:58:02 +0100

    correction d'un bug (self manquant)

--- src/vecteur.py
+++ src/vecteur.py
@@ -65,7 +65,7 @@ class vecteur:
         return vecteur(x, y)
 
     def __eq__(self, v):
-        return x == v.x and y == v.y
+        return self.x == v.x and self.y == v.y
     
     def __sub__(self, v):
         x = self.x - v.x
