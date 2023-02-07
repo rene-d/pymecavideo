a9467e7cac0a25df7bb3315559eeb7e7913d2597
Georges Khaznadar <georgesk@debian.org>
2023-02-07 10:51:18 +0100

    supprimé le lien videowidget -> spinBox_image

--- src/videoWidget.py
+++ src/videoWidget.py
@@ -118,7 +118,7 @@ class VideoPointeeWidget(ImageWidget, Pointage):
         # réplication de certains attributs de la fenêtre principale
         attributes = [
             "dbg",
-            "spinBox_image", "spinBox_nb_de_points", "spinBox_chrono",
+            "spinBox_nb_de_points", "spinBox_chrono",
             "Bouton_lance_capture", "Bouton_Echelle", "tabWidget",
             "graphWidget", "tableWidget", "tab_traj", "comboBox_referentiel",
             "pushButton_select_all_table", "pushButton_origine",
