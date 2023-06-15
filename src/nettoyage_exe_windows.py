954822ba0e0968d9c504d808ead807833e130598
JB BUTET <ashashiwa@gmail.com>
2023-06-15 11:50:26 +0200

    Nettoyage exe windows

--- src/nettoyage_exe_windows.py
+++ src/nettoyage_exe_windows.py
@@ -125,7 +125,7 @@ os.chdir(dossier_du_build)
 for rep, dossier_fichier in a_enlever.items():
     try:
         for fichier_ in dossier_fichier:
-            print(f"effacement de {fichier_}")
+            print(f"effacement de {fichier_}, dans {dossier_fichier}")
             os.remove(os.path.join(rep, fichier_))
 
     except FileNotFoundError :
